76f9048b2994aa5108261b3f6583584f3ab7cd04
huongduyen <duyen.vanthi.cit20@eiu.edu.vn>
2024-01-26 21:49:10 +0700

    Create use case
    Finding user class

---
+++ TalkApplication/src/main/java/application/talk/usecases/user/FindingUser.java
@@ -0,0 +1,55 @@
+package application.talk.usecases.user;
+
+import application.talk.usecases.UseCase;
+import application.talk.usecases.adapters.DataStorage;
+
+public class FindingUser extends UseCase<FindingUser.InputValues, FindingUser.OutputValues> {
+	private DataStorage _dataStorage;
+
+	public FindingUser(DataStorage dataStorage) {
+		_dataStorage = dataStorage;
+	}
+
+	@Override
+	public OutputValues execute(InputValues input) {
+		_dataStorage.getUsers().getByName(input.getName());
+		return new OutputValues(FindingResult.SUCCESSFUL, "");
+	}
+
+	public static class InputValues {
+		private String _username;
+
+		public InputValues(String username) {
+			super();
+			_username = username;
+		}
+		
+		public String getName() {
+			return _username;
+		}
+
+	}
+
+	public static class OutputValues {
+		private final FindingResult RESULT;
+		private final String MESSAGE;
+
+		public OutputValues(FindingResult result, String message) {
+			MESSAGE = message;
+			RESULT = result;
+		}
+
+		public FindingResult getResult() {
+			return RESULT;
+		}
+
+		public String getMessage() {
+			return MESSAGE;
+		}
+	}
+
+	public static enum FindingResult {
+		SUCCESSFUL, FAILED;
+	}
+
+}
