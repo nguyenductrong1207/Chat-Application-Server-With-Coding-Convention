7197f8bbce0bdee5aed0e3106d939e8419ac2aae
huongduyen <duyen.vanthi.cit20@eiu.edu.vn>
2024-01-27 08:40:04 +0700

    create function
    creating group

--- TalkApplication/src/main/java/application/talk/usecases/user/CreatingGroup.java
+++ TalkApplication/src/main/java/application/talk/usecases/user/CreatingGroup.java
@@ -1,5 +1,33 @@
 package application.talk.usecases.user;
 
+import java.util.UUID;
+
+import application.talk.domains.User;
+
 public class CreatingGroup {
+	private boolean _isPublic;
+	private String _joinCode;
+	private User _admin;
+
+	public CreatingGroup(boolean isPublic, String joinCode, User admin) {
+		super();
+		_isPublic = isPublic;
+		_joinCode = joinCode;
+		_admin = admin;
+
+		if (isPublic) {
+			generateCode();
+		} else {
+			createAdmin(admin);
+		}
+	}
+
+	public void generateCode() {
+		_joinCode = UUID.randomUUID().toString().substring(0, 6);
+	}
+
+	public void createAdmin(User user) {
+		_admin = user;
+	}
 
 }
