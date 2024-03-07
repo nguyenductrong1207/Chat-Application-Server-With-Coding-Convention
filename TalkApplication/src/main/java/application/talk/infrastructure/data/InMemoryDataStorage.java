c54d7ca4d070e46d362ed1702575055ca0c7df36
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2024-03-08 00:10:58 +0700

    Fix JoiningGroup

--- TalkApplication/src/main/java/application/talk/infrastructure/data/InMemoryDataStorage.java
+++ TalkApplication/src/main/java/application/talk/infrastructure/data/InMemoryDataStorage.java
@@ -6,67 +6,73 @@ import application.talk.usecases.adapters.DataStorage;
 import application.talk.usecases.adapters.Repository;
 
 public class InMemoryDataStorage implements DataStorage {
-	private Repository<User> _users;
-	private Repository<Group> _groups;
-	private Repository<Message> _messages;
+    private Repository<User> _users;
+    private Repository<Group> _groups;
+    private Repository<Message> _messages;
+    private Repository<Conversation> _conversation;
+    private Repository<Request> _request;
     private static InMemoryDataStorage storage;
-	
-	private InMemoryDataStorage() {
-		_users =  new InMemoryRepository<User>();
-		_groups = new InMemoryRepository<Group>();
-		_messages = new InMemoryRepository<Message>();
-	}
-	
-	public static synchronized InMemoryDataStorage getInstance() {
+
+    private InMemoryDataStorage() {
+        _users = new InMemoryRepository<User>();
+        _groups = new InMemoryRepository<Group>();
+        _messages = new InMemoryRepository<Message>();
+        _conversation = new InMemoryRepository<Conversation>();
+        _request = new InMemoryRepository<Request>();
+    }
+
+    public static synchronized InMemoryDataStorage getInstance() {
         if (storage == null) {
             storage = new InMemoryDataStorage();
         }
-        
+
         return storage;
     }
 
-	@Override
-	public Repository<User> getUsers() {
-		return _users;
-	}
+    @Override
+    public Repository<User> getUsers() {
+        return _users;
+    }
 
-	@Override
-	public Repository<Group> getGroups() {
-		return _groups;
-	}
-	
-	@Override
-	public Repository<Message> getMessages() {
-		return _messages;
-	}
+    @Override
+    public Repository<Group> getGroups() {
+        return _groups;
+    }
 
-	@Override
-	public Repository<Conversation> getConversations() {
-		return null;
-	}
+    @Override
+    public Repository<Message> getMessages() {
+        return _messages;
+    }
 
-	@Override
-	public Repository<Request> getRequests() {
-		return null;
-	}
+    @Override
+    public Repository<Conversation> getConversations() {
+        return _conversation;
+    }
 
-	@Override
-	public void cleanAll() {
-		_users.deleteAll();
-		_groups.deleteAll();
-		_messages.deleteAll();
-	}
+    @Override
+    public Repository<Request> getRequests() {
+        return _request;
+    }
 
-	@Override
-	public boolean remove(Message message) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void cleanAll() {
+        _users.deleteAll();
+        _groups.deleteAll();
+        _messages.deleteAll();
+        _conversation.deleteAll();
+        _request.deleteAll();
+    }
 
-	@Override
-	public Repository<ChatEntity> getChatEntities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean remove(Message message) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Repository<ChatEntity> getChatEntities() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
---
+++ TalkApplication/src/test/java/application/talk/usecases/user/JoiningPrivateGroupTest.java
@@ -0,0 +1,40 @@
+package application.talk.usecases.user;
+
+import application.talk.domains.User;
+import application.talk.infrastructure.data.InMemoryDataStorage;
+import application.talk.usecases.adapters.DataStorage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JoiningPrivateGroupTest {
+
+    DataStorage _storage;
+    JoiningPrivateGroup _useCase;
+
+    @Before
+    public void setUp() throws Exception {
+        _storage = InMemoryDataStorage.getInstance();
+        _useCase = new JoiningPrivateGroup(_storage);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        _storage.cleanAll();
+    }
+
+    @Test
+    public void testJoiningPrivateGroup() {
+        User inviter = new User("trong", "1207");
+        User receiver = new User("trong2", "1910");
+
+        JoiningPrivateGroup.InputValues input = new JoiningPrivateGroup.InputValues(inviter, receiver);
+        JoiningPrivateGroup.OutputValues output = _useCase.execute(input);
+
+        assertEquals(JoiningPrivateGroup.JoiningPrivateGroupResult.SUCCESSFUL, output.getResult());
+    }
+
+
+}
